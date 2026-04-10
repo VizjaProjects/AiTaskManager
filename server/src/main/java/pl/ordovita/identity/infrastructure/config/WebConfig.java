a1b34b0b87c4d172cbadb7432e0028cfab7394d7
EDS3L <mBarlik84@gmail.com>
2026-04-10 18:50:02 +0200

    fix oauth

--- server/src/main/java/pl/ordovita/identity/infrastructure/config/WebConfig.java
+++ server/src/main/java/pl/ordovita/identity/infrastructure/config/WebConfig.java
@@ -17,7 +17,6 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import pl.ordovita.identity.domain.port.AuthenticatedUser;
 import pl.ordovita.identity.infrastructure.jwt.JwtAuthenticationEntryPoint;
 import pl.ordovita.identity.infrastructure.jwt.JwtAuthenticationFilter;
 import pl.ordovita.identity.infrastructure.security.CorsConfig;
@@ -28,23 +27,20 @@ import pl.ordovita.identity.infrastructure.security.CorsConfig;
 public class WebConfig {
 
     private final CorsConfig corsConfig;
-    private final JwtAuthenticationEntryPoint  jwtAuthenticationEntryPoint;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
-                .csrf(AbstractHttpConfigurer::disable)
-                .oauth2Login(oauth -> oauth.userInfoEndpoint(userInfo -> userInfo.userService(oauth2UserService())))
-                .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .headers( headers -> headers.httpStrictTransportSecurity(HeadersConfigurer.HstsConfig::disable))
-                .authorizeHttpRequests( auth -> auth
-                        .requestMatchers("/v1/api/auth/**", "/v1/api/emailVerification/**").permitAll()
-                        .requestMatchers("/swagger-ui/*", "/v3/api-docs/**").permitAll()
-                        .anyRequest().authenticated()
-                );
+        http.cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource())).csrf(AbstractHttpConfigurer::disable).oauth2Login(
+                oauth -> oauth.userInfoEndpoint(userInfo -> userInfo.userService(oauth2UserService())).successHandler(
+                        authenticationSuccessHandler())).exceptionHandling(exception -> exception.authenticationEntryPoint(
+                jwtAuthenticationEntryPoint)).sessionManagement(session -> session.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS)).headers(headers -> headers.httpStrictTransportSecurity(
+                HeadersConfigurer.HstsConfig::disable)).authorizeHttpRequests(auth -> auth.requestMatchers(
+                "/v1/api/auth/**",
+                "/v1/api/emailVerification/**").permitAll().requestMatchers("/swagger-ui/*",
+                "/v3/api-docs/**").permitAll().anyRequest().authenticated());
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
